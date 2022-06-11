d9bf3e0715789df1585c5b8b150b6ae9093002d1
tissyane <tissyane@gmail.com>
2022-06-11 15:52:06 -0300

    botão fechar o pedido

--- scripts.js
+++ scripts.js
@@ -1,12 +1,54 @@
-function selecionado (objeto, categoria) {
+function selecionado(objeto, categoria) {
+  const elemento = document.querySelector(`.${categoria}.escolhido`);
 
-    const elemento = document.querySelector(`.${categoria}.escolhido`);
-    
-    if (elemento != null) {
-        elemento.classList.remove("escolhido");
-    }
+  if (elemento !== null) {
+    elemento.classList.remove("escolhido");
+  }
 
-    objeto.classList.add("escolhido")
+  objeto.classList.add("escolhido");
+  checarBotao();
 }
 
+function checarBotao() {
+  if (!isBtnActive) {
+    prato = document.querySelector(".prato.escolhido");
+    bebida = document.querySelector(".bebida.escolhido");
+    sobremesa = document.querySelector(".sobremesa.escolhido");
+    const botao = document.querySelector("footer div");
+    isBtnActive = prato !== null && bebida !== null && sobremesa !== null;
+
+    if (isBtnActive) {
+      botao.classList.remove("off");
+      botao.innerHTML = "Fechar pedido";
+    }
+  }
+}
+
+let prato = null
+let bebida = null;
+let sobremesa = null;
+let isBtnActive = false;
+
+function finalizarPedido () {
+    if (isBtnActive) {
+    const prato_nome = ('.prato .escolhido .item_title p' ).innerHTML;
+    let prato_preco = '.prato span';
+    let bebida_nome = '.bebida item_title';
+    let bebida_preco = '.bebida span';
+    let sobremesa_nome = '.sobremesa item_title';
+    let sobremesa_preco = '.sobremesa span';
+    const mensagem = `
+            Olá, gostaria de fazer o pedido:
+                Prato: ${prato_nome}
+                Bebida: ${bebida_nome}
+                Sobremesa: ${sobremesa_nome}`
+    alert(mensagem);
+    }
+
+    
+
+
+
+
+}
 
